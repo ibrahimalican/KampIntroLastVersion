a1930c9340b29dc21212fd98d17a1c4a2d9fdf24
abutun11 <butunalican01@gmail.com>
2024-07-06 18:04:59 +0300

    Add project files.

---
+++ KampIntroLastVersion/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace KampIntroLastVersion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            // do not repeat yourself -- kendini tekrarlama
+            // değer tutucu -- alias
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugün = 7.55;
+
+
+            if (dolarDun > dolarBugün)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugün)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+
+
+            if (sistemeGirisYapmisMi)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
