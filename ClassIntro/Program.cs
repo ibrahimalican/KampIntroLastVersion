f3e381a1ffce1918b0f18ed3ca0fdab8cf903d88
abutun11 <butunalican01@gmail.com>
2024-07-07 17:52:52 +0300

    07.07.2024 - İkinci Ders Kodları

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -43,6 +43,56 @@
 
 
             Console.WriteLine("Hello, World!");
+
+
+
+            Product product1 = new Product();
+            product1.Category = "Beyaz Eşya";
+            product1.Name = "Buzdolabı";
+            product1.Price = 6999.99;
+            product1.StockQuantity = 25;
+
+
+            Product product2 = new Product();
+            product2.Category = "Akıllı Telefon";
+            product2.Name = "IPhone 15 Pro Max";
+            product2.Price = 89999.00;
+            product2.StockQuantity = 50;
+
+
+            Product product3 = new Product();
+            product3.Category = "Dizüstü Bilgisayar";
+            product3.Name = "Lenovo";
+            product3.Price = 36500.00;
+            product3.StockQuantity = 8;
+
+
+            Product product4 = new Product();
+            product4.Category = "Giyim";
+            product4.Name = "Yazlık Erkek Gömlek";
+            product4.Price = 650.00;
+            product4.StockQuantity = 120;
+
+
+            Product[] products = new Product[] {product1, product2, product3, product4 };
+
+            Console.WriteLine("for başladı");
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].Category + " - " + products[i].Name + " - " + products[i].Price + " - " + products[i].StockQuantity);
+            }
+            Console.WriteLine("foreach başladı");
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Category + " - " + product.Name + " - " + product.Price + " - " + product.StockQuantity);
+            }
+            Console.WriteLine("while başladı");
+            int a = 0;
+            while (a < products.Length)
+            {
+                Console.WriteLine(products[a].Category + " - " + products[a].Name + " - " + products[a].Price + " - " + products[a].StockQuantity);
+                a++;
+            }
         }
     }
 
@@ -52,5 +102,14 @@
         public string Egitmen { get; set; }
         public int IzlenmeOrani { get; set; }
 
+    }
+
+
+    class Product
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int StockQuantity { get; set; }
     }
 }
---
+++ Matematik/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Matematik
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
+
+            dortIslem.Topla(6, 9);
+        }
+    }
+}
