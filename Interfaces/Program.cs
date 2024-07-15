7fffc016f2d99578638c76b2a06d2eb4181831ee
abutun11 <butunalican01@gmail.com>
2024-07-16 00:13:27 +0300

    16.07.2024 - 4. Ders Ödev Kodları

---
+++ Interfaces/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Interfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleCustomerDal());
+
+            IPerson person = new Worker();
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            manager.Add(new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara" });
+
+            Student student = new Student
+            {
+                Id = 2,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Department = "Computer Sciences"
+            };
+            manager.Add(student);
+            manager.Add(new Worker { });
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
+    class PersonManager 
+    {
+        public void Add(IPerson person) 
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
---
+++ Interfaces2/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interface new'lenemez
+            //IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            //employeeManager.Add();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.AddCustomer(new InternManager());
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+        
+    }
+
+    //inherits -> class ------------- implements -> interface
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //müşteri ekleme kodları
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //personel ekleme kodları
+            Console.WriteLine("Personel eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void AddCustomer(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+
+    }
+}
