86c504ffce7406443802efd517feef4b26eff996
abutun11 <butunalican01@gmail.com>
2024-07-28 15:17:13 +0300

    28.07.2024 - 5. Ders Ödev Kodları

---
+++ GameProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GameProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1985, FirstName = "ENGİN", LastName = "DEMİROĞ", IdentityNumber = 12345 });
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
